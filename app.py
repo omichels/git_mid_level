d97a9d52a6741f4d171631bc69d43ae159ec930e
Oliver Michels <oliver.michels@gmail.com>
2023-03-28 08:12:49 +0200

    .

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 
-import time
+import time, logging
 from flask import Flask
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
 START = time.time()
@@ -10,11 +12,12 @@ def elapsed():
     running = time.time() - START
     minutes, seconds = divmod(running, 60)
     hours, minutes = divmod(minutes, 60)
+    logger.debug("DEBUG TOKEN SWORDFISH %s", seconds)
     return "%d:%02d:%02d" % (hours, minutes, seconds)
 
 @app.route('/')
 def root():
-    return "Top-Security Information needed is here: (up %s)\n" % elapsed()
+    return "DEBUG Top-Security Information needed is here: (up %s)\n" % elapsed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
